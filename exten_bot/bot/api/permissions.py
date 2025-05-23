902eb6ae9ab6648d0df984710807922e7bb31108
gulin <antgulin@ya.ru>
2025-05-23 17:59:01 +0500

    HTTP_X_REAL_IP

--- exten_bot/bot/api/permissions.py
+++ exten_bot/bot/api/permissions.py
@@ -6,7 +6,7 @@ from rest_framework.permissions import BasePermission
 class AllowTrustedIpOrAuthenticated(BasePermission):
     def has_permission(self, request, view):
         trusted_ips = os.environ.get("TRUSTED_IPS", "127.0.0.1").split(",")
-        ip_addr = request.META.get("REMOTE_ADDR")
+        ip_addr = request.META.get("HTTP_X_REAL_IP")
         if ip_addr in trusted_ips:
             return True
         return request.user and request.user.is_authenticated
